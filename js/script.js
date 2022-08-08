b5af97b88d32081039b76fa9adb8d08bcbe89e5f
Daniel Bilodid <danielbeloded@gmail.com>
2022-08-08 14:05:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = require('./modules/tabs'),
+        modal = require('./modules/modal'),
+        timer = require('./modules/timer'),
+        cards = require('./modules/cards'),
+        slider = require('./modules/slider'),
+        calculator = require('./modules/calculator'),
+        forms = require('./modules/forms');
+
+    tabs();
+    modal();
+    timer();
+    cards();
+    slider();
+    calculator();
+    forms();
+
+});
